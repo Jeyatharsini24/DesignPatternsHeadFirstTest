28bf02c728b0c9d0d73ccb23bf15a3c0b4b3d308
JJeyatharsini <jeyatharsini.11@cse.mrt.ac.lk>
2018-01-17 19:33:22 +0530

    Added code for CD cover example in book

---
+++ src/com/jeya/proxy/virtualproxyexampleinbook/ImageComponent.java
@@ -0,0 +1,22 @@
+package com.jeya.proxy.virtualproxyexampleinbook;
+
+import javax.swing.Icon;
+import javax.swing.JComponent;
+
+import com.jeya.proxy.virtualproxy.ImageProxy;
+
+public class ImageComponent extends JComponent
+{
+
+  public ImageComponent(Icon icon)
+  {
+    // TODO Auto-generated constructor stub
+  }
+
+  public void setIcon(ImageProxy imageProxy)
+  {
+    // TODO Auto-generated method stub
+    
+  }
+
+}
---
+++ src/com/jeya/proxy/virtualproxyexampleinbook/ImageProxyTestDrive.java
@@ -0,0 +1,80 @@
+package com.jeya.proxy.virtualproxyexampleinbook;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Enumeration;
+import java.util.Hashtable;
+
+import javax.swing.Icon;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
+import com.jeya.proxy.virtualproxy.ImageProxy;
+
+public class ImageProxyTestDrive
+{
+  private ImageComponent imageComponent;
+
+  private JFrame frame = new JFrame("CD Cover Viewer");
+
+  private JMenuBar menuBar;
+
+  private JMenu menu;
+
+  private Hashtable<String, String> cds = new Hashtable<>();
+
+  public static void main(String[] args) throws MalformedURLException
+  {
+    ImageProxyTestDrive imageProxyTestDrive = new ImageProxyTestDrive();
+  }
+
+  private ImageProxyTestDrive() throws MalformedURLException
+  {
+    cds.put("Ambient: Music for Airports", "http://images.amazon.com/images/P/B000003S2K.01.LZZZZZZZ.jpg");
+    URL initiateURL = new URL(cds.get("Ambient: Music for Airports"));
+    menuBar = new JMenuBar();
+    menu = new JMenu("Favorite CDs");
+    menuBar.add(menu);
+    frame.setJMenuBar(menuBar);
+
+    for (Enumeration e = cds.keys(); e.hasMoreElements();)
+    {
+      String name = (String) e.nextElement();
+      JMenuItem menuItem = new JMenuItem(name);
+      menu.add(menuItem);
+      menuItem.addActionListener(new ActionListener()
+      {
+        @Override
+        public void actionPerformed(ActionEvent e)
+        {
+          imageComponent.setIcon(new ImageProxy(getCDUrl(e.getActionCommand())));
+          frame.repaint();
+        }
+      });
+    }
+
+    Icon icon = new ImageProxy(initiateURL);
+    imageComponent = new ImageComponent(icon);
+    frame.getContentPane().add(imageComponent);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setSize(800, 600);
+    frame.setVisible(true);
+  }
+
+  private URL getCDUrl(String name)
+  {
+    try
+    {
+      return new URL(cds.get(name));
+    }
+    catch (MalformedURLException e)
+    {
+      e.printStackTrace();
+      return null;
+    }
+  }
+}
